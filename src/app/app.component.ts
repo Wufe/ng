12a4c8ff35cae083093558c8eabf02a0ee22a0d4
Simone Bembi <wufe92@gmail.com>
2017-03-27 22:35:53 +0000

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1 [ngStyle]="{'color':color}">{{title}}</h1>
+    <br />
+  
+    <div [ngStyle]="{'display':'inline-block'}">
+        <input #inputBox class="form-control form-inline" (keyup)="test($event)" value="{{inputText}}" />
+    </div>
+  
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Test!';
+  color: string = "navy";
+  inputText: string = "navy"
+  inputBox: any;
+  
+  test(event: any){
+    this.color = event.target.value;
+  }
 }
