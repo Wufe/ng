526e78e3fa818ac5577e8d9578107004d388ef88
Simone Bembi <wufe92@gmail.com>
2017-03-28 18:06:32 +0000

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,67 @@
 import { Component } from '@angular/core';
 
+type Stage = {
+	done: boolean;
+	title: string;
+};
+
 @Component({
-  selector: 'app-root',
-  template: `<h1 [ngStyle]="{'color':color}">{{title}}</h1>
-    <br />
-  
-    <div [ngStyle]="{'display':'inline-block'}">
-        <input #inputBox class="form-control form-inline" (keyup)="test($event)" value="{{inputText}}" />
-    </div>
-  
-  `,
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	template: `
+		<div class="container">
+			<div class="row header">
+				<header>
+					<h1>Learning path</h1>
+				</header>
+			</div>
+			<div class="row">
+				<div class="col-md-offset-1 col-md-10">
+					<ul class="list-group" *ngFor="let stage of stages">
+						<li class="well well-sm list-group-item">
+							<div>
+								<input type="checkbox" [checked]="stage.done" /> <span class="stage-item">{{ stage.title }}</span>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-offset-7 col-md-4 form form-inline">
+					<div class="form-group">
+						<input id="addStage" [(ngModel)]="stageInput" class="form-control" (keyup.enter)="onEnter()" placeholder="Aggiungi una tappa" />
+						<button class="btn btn-info" (click)="onEnter()">Aggiungi</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		
+		`,
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Test!';
-  color: string = "navy";
-  inputText: string = "navy"
-  inputBox: any;
-  
-  test(event: any){
-    this.color = event.target.value;
-  }
+	title = 'Test!';
+	color: string = "navy";
+	
+	stages: Stage[] = [
+		{
+			done: true,
+			title: "Folder Structure"
+		}
+	];
+	
+	stageInput = "";
+	
+	onEnter(){
+		console.log( this.stageInput );
+		this.stages = [
+			...this.stages,
+			{
+				done: false,
+				title: this.stageInput
+			}
+		];
+		this.stageInput = "";
+	}
+	
 }
